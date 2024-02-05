f4b2ef255db2520b34c9e4fcf633e4c7757bc132
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-05 23:18:24 +0600

    Time: 72 ms (56.52%), Space: 42.2 MB (48.22%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs == null || strs.Length == 0)
+            return "";
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            int j = 0;
+            while (j < prefix.Length && j < strs[i].Length && prefix[j] == strs[i][j])
+            {
+                j++;
+            }
+
+            prefix = prefix.Substring(0, j);
+
+            if (string.IsNullOrEmpty(prefix))
+                return "";
+        }
+
+        return prefix;
+    }
+}
