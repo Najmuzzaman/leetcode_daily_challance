06655753b850a9f3fc0b22881e1cb48774eabc22
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-07 14:21:08 +0600

    Time: 49 ms (98.88%), Space: 43.7 MB (49.16%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cs
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cs
@@ -1,24 +1,25 @@
 public class Solution {
     public string FrequencySort(string s) {
-        Dictionary<char,int> mp = new Dictionary<char,int>();
-        foreach (char c in s)
-        {
-            if(mp.ContainsKey(c))
-            {
-                mp[c]++;
-            }
-            else
-            {
-                mp[c] = 1;
-            }
+       int[] mp = new int[128];
+        // Calculate frequencies of characters
+        foreach (char c in s) {
+            mp[c]++;
         }
+
+        // Create an array of characters from ASCII values
+        char[] chars = Enumerable.Range(0, 128).Select(i => (char)i).ToArray();
+
+        // Sort characters by frequency in descending order
+        Array.Sort(chars, (a, b) => mp[b] - mp[a]);
+
         // Create a StringBuilder to efficiently build the result string
         StringBuilder result = new StringBuilder();
 
-        // Iterate over the dictionary in descending order of frequency
-        foreach (var kvp in mp.OrderByDescending(x => x.Value)) {
-            // Append the character kvp.Key to the result string kvp.Value times
-            result.Append(kvp.Key, kvp.Value);
+        // Construct the result string
+        foreach (char c in chars) {
+            if (mp[c] > 0) {
+                result.Append(c, mp[c]);
+            }
         }
 
         return result.ToString();
