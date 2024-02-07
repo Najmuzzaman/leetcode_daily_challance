521501ab3fc9e66b12558c7bee35db507aca3f9a
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-07 14:51:26 +0600

    Time: 110 ms (70.00%), Space: 47 MB (71.67%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cs
@@ -2,27 +2,19 @@ public class Solution {
     public int[] FrequencySort(int[] nums) {
         int n = nums.Length;
         int[] mp = new int[201];
-        List<int> ch=new List<int>();
+
         // Calculate frequencies of characters
-        for(int i=0;i<n;i++)
+        foreach (int num in nums)
         {
-            if(mp[nums[i]+100]==0) ch.Add(nums[i]+100);
-            mp[nums[i]+100]++;
+            mp[num + 100]++;
         }
-        int[] chars=ch.ToArray();
-        Array.Sort(chars, (a, b) => {
-        int cmp = mp[a] - mp[b]; // Compare frequencies in asc order
-        return cmp == 0 ? b - a : cmp; // If frequencies are equal, sort by character value
+
+        // Sort frequencies in ascending order
+        Array.Sort(nums, (a, b) => {
+            int freqDiff = mp[a + 100] - mp[b + 100]; // Compare frequencies in ascending order
+            return freqDiff == 0 ? b - a : freqDiff; // If frequencies are equal, sort by character value
         });
-        int j = 0;
-        foreach (int c in chars)
-        {
-            while (mp[c] != 0)
-            {
-                nums[j++] = c-100;
-                mp[c]--;
-            }
-        }
+
         return nums;
     }
 }
