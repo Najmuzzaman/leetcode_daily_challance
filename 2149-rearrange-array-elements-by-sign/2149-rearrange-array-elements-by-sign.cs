278abceca7de6385f54f6f38f882fa9239753f18
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-14 11:29:12 +0600

    Time: 482 ms (85.71%), Space: 134.8 MB (55.10%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cs
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cs
@@ -2,27 +2,20 @@ public class Solution {
     public int[] RearrangeArray(int[] nums) {
         int n = nums.Length;
         int[] result = new int[n];
-        List<int> positive=new List<int>();
-        List<int> negative=new List<int>();
-        for (int i=0;i<n;i++)
-        {
-            if (nums[i]>0)
-                positive.Add(nums[i]);
-            else 
-                negative.Add(nums[i]);
+        int positiveIndex = 0;
+        int negativeIndex = 1;
+
+         for(int i=0;i<n;i++) 
+         {
+            if (nums[i] > 0) {
+                result[positiveIndex] = nums[i];
+                positiveIndex += 2;
+            } else {
+                result[negativeIndex] = nums[i];
+                negativeIndex += 2;
+            }
         }
-        int ind= 0;
-        foreach (int i in positive )
-        {
-            result[ind] = i;
-            ind+=2;
-        }
-        ind = 1;
-        foreach (int i in negative)
-        {
-            result[ind] = i;
-            ind += 2;
-        }
+
         return result;
     }
 }
