3bf44bc4f33e29bae127a11323320cfde10f949d
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-14 11:30:23 +0600

    Time: 483 ms (83.67%), Space: 137 MB (20.41%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cs
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cs
@@ -4,18 +4,19 @@ public class Solution {
         int[] result = new int[n];
         int positiveIndex = 0;
         int negativeIndex = 1;
-
-         for(int i=0;i<n;i++) 
-         {
-            if (nums[i] > 0) {
+        for(int i=0;i<n;i++) 
+        {
+            if (nums[i] > 0)
+            {
                 result[positiveIndex] = nums[i];
                 positiveIndex += 2;
-            } else {
+            } 
+            else
+            {
                 result[negativeIndex] = nums[i];
                 negativeIndex += 2;
             }
         }
-
         return result;
     }
 }
