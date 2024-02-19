af61aa27be738e3a27c32949ce5bc5858604abdc
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-19 15:42:54 +0600

    Time: 30 ms (61.92%), Space: 28.8 MB (43.21%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cs
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -1,9 +1,5 @@
 public class Solution {
     public bool IsPowerOfTwo(int n) {
-        if (n==1) return true;
-        if (n%2==1) return false;
-        if(n<1) return false;
-        int count = BitOperations.PopCount((uint)n);
-        return count==1;
+         return n>0 && (n & -n) == n;
     }
 }
