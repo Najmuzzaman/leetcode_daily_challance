fb9530c7ee62d90bb4456c5c86026963df289eb3
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-02-21 21:44:50 +0600

    Time: 38 ms (13.71%), Space: 27.7 MB (53.23%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cs
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public int RangeBitwiseAnd(int left, int right) {
         int shift = 0;
-
         // Find the common prefix (MSB) of left and right
         while (left < right) {
             left >>= 1;
             right >>= 1;
             shift++;
         }
-
         // Shift left by the common prefix to get the final result
         return left << shift;
     }
