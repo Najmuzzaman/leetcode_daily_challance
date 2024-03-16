32cf3fd2a8fa6ecb3803fde50e0f5850a67696c6
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-03-15 22:32:50 -0700

    Time: 251 ms (71.73%), Space: 51.7 MB (94.76%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int FindMaxLength(int[] nums) {
+        int ans = 0;
+        Dictionary<int, int> SumMap = new Dictionary<int, int> {{0, -1}};
+        int n=nums.Length;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            count += (nums[i] == 1) ? 1 : -1;            
+            if (SumMap.ContainsKey(count))
+                ans = Math.Max(ans, i - SumMap[count]);
+            else
+                SumMap.Add(count, i);
+        }        
+        return ans;
+    }
+}
