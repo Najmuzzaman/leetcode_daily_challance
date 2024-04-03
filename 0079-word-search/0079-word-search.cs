0dcbf9b22fd9e81c82b50a61a89309d11cc8dc6c
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-04-02 19:59:09 -0700

    Time: 353 ms (51.55%), Space: 42.5 MB (38.86%) - LeetHub

--- 0079-word-search/0079-word-search.cs
+++ 0079-word-search/0079-word-search.cs
@@ -1,18 +1,19 @@
 public class Solution {
     char[][] board;
+    bool[,] visited;
     int len,m,n; 
     public bool Exist(char[][] board, string word) {
         this.board = board;
         m = board.Length;
         n = board[0].Length;
         len=word.Length;
-        bool[,] visited = new bool[m, n];
+        visited = new bool[m, n];
         bool ans = false;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (this.board[i][j] == word[0]) {
-                    ans = dfs(word, visited, i, j, 0);
+                    ans = dfs(word, i, j, 0);
                     if (ans) return true;
                 }
             }
@@ -21,21 +22,21 @@ public class Solution {
         return false;
     }
     
-    private bool dfs(string word, bool[,] visited, int i, int j, int v) {
+    private bool dfs(string word, int i, int j, int v) {
         if (v == len) {
             return true;
         }
         
-        if (!IsValid(i, j, visited, word[v])) {
+        if (!IsValid(i, j, word[v])) {
             return false;
         }
         
         visited[i, j] = true;
         
-        if (dfs(word, visited, i + 1, j, v + 1) ||
-            dfs(word, visited, i - 1, j, v + 1) ||
-            dfs(word, visited, i, j + 1, v + 1) ||
-            dfs(word, visited, i, j - 1, v + 1)) {
+        if (dfs(word, i + 1, j, v + 1) ||
+            dfs(word, i - 1, j, v + 1) ||
+            dfs(word, i, j + 1, v + 1) ||
+            dfs(word, i, j - 1, v + 1)) {
             return true;
         }
         
@@ -43,7 +44,7 @@ public class Solution {
         return false;
     }
     
-    private bool IsValid(int i, int j, bool[,] visited, char target) {               
+    private bool IsValid(int i, int j, char target) {               
         return i >= 0 && i < m && j >= 0 && j < n && !visited[i, j] && board[i][j] == target;
     }
 }
