c0d1d7a6a1371374910278e34d84f31bddc82f17
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-04-20 00:40:35 -0700

    Time: 402 ms (78.57%), Space: 95.4 MB (35.71%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cs
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cs
@@ -4,39 +4,40 @@ public class Solution {
         int m = land.Length;
         int n = land[0].Length;
 
-        Func<int, int, int[]> findFarmlandCoordinates = (row, col) => {
-            int[] coordinates = new int[4];
-            int r = row, c = col;
-
-            while (r < m && land[r][col] == 1) {
-                r++;
-            }
-            while (c < n && land[row][c] == 1) {
-                c++;
-            }
-
-            coordinates[0] = row;
-            coordinates[1] = col;
-            coordinates[2] = r - 1;
-            coordinates[3] = c - 1;
-
-            for (int i = row; i < r; i++) {
-                for (int j = col; j < c; j++) {
-                    land[i][j] = 0;
-                }
-            }
-
-            return coordinates;
-        };
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (land[i][j] == 1) {
-                    result.Add(findFarmlandCoordinates(i, j));
+                    result.Add(findFarmlandCoordinates(land, i, j, m, n));
                 }
             }
         }
 
         return result.ToArray();
+    }
+    
+     private int[] findFarmlandCoordinates(int[][] land, int row, int col, int m, int n) {
+        int[] coordinates = new int[4];
+        int r = row, c = col;
+
+        while (r < m && land[r][col] == 1) {
+            r++;
+        }
+        while (c < n && land[row][c] == 1) {
+            c++;
+        }
+
+        coordinates[0] = row;
+        coordinates[1] = col;
+        coordinates[2] = r - 1;
+        coordinates[3] = c - 1;
+
+        for (int i = row; i < r; i++) {
+            for (int j = col; j < c; j++) {
+                land[i][j] = 0;
+            }
+        }
+
+        return coordinates;
     }
 }
