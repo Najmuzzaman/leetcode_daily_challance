fab9398570333933fd682bfc54cb246edde56be7
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-05-04 00:39:09 -0700

    Time: 181 ms (97.80%), Space: 51.1 MB (10.99%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cs
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cs
@@ -1,21 +1,16 @@
 public class Solution {
-    public int NumRescueBoats(int[] people, int limit) {
+    public int NumRescueBoats(int[] people, int limit) 
+    {
         Array.Sort(people);
         int i=0 , boats=0;
         int j=people.Length-1;
-
-        while(i<=j){
-            if(people[i]+people[j]<=limit){
+        while(i<=j)
+        {
+            if(people[i]+people[j]<=limit)
                 i++;
-                j--;
-                boats++;
-            }
-            else{
-                boats++;
-                j--;
-            }
+            boats++;
+            j--;
         }
-
         return boats;
     }
 }
