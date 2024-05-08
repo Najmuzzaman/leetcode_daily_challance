8ccc35cbd5c3ed4b7e321115886c1ce2188f4e0b
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-05-07 22:46:24 -0700

    Time: 140 ms (66.67%), Space: 69.4 MB (81.56%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cs
+++ 0506-relative-ranks/0506-relative-ranks.cs
@@ -1,26 +1,36 @@
 public class Solution {
     public string[] FindRelativeRanks(int[] score) {
-        int n=score.Length;
-        string[] ans=new string[n];        
-        int[] s = new int[n];
-        Array.Copy(score, s, n);
-        Array.Sort(s);
-        Array.Reverse(s);
-        int[] ind=new int[1000001];
-        for(int i=0;i<n;i++)    
-            ind[s[i]]=i+1;
+        int n = score.Length;
+        string[] ans = new string[n];
         
-        for(int i=0;i<n;i++)
-        {
-            if(ind[score[i]]==1)
-                ans[i]="Gold Medal";
-            else if(ind[score[i]]==2)
-                ans[i]="Silver Medal";
-            else if(ind[score[i]]==3)
-                ans[i]="Bronze Medal";
-            else
-                ans[i]=ind[score[i]].ToString();
+        // Find the maximum score
+        int maxScore = int.MinValue;
+        foreach (int s in score) {
+            maxScore = Math.Max(maxScore, s);
         }
+        
+        // Create an array to store indices of scores
+        int[] index = new int[maxScore + 1];
+        for (int i = 0; i < n; i++) {
+            index[score[i]] = i + 1;
+        }
+        
+        // Assign ranks
+        int rank = 1;
+        for (int i = maxScore; i >= 0; i--) {
+            if (index[i] != 0) {
+                if (rank == 1)
+                    ans[index[i] - 1] = "Gold Medal";
+                else if (rank == 2)
+                    ans[index[i] - 1] = "Silver Medal";
+                else if (rank == 3)
+                    ans[index[i] - 1] = "Bronze Medal";
+                else
+                    ans[index[i] - 1] = rank.ToString();
+                rank++;
+            }
+        }
+        
         return ans;
     }
 }
