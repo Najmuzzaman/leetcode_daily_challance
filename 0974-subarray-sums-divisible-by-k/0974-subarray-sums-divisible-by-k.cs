ec1eb529b80bbfff251b703fccfed5ec8d69d180
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-06-09 14:04:50 +0600

    Time: 151 ms (46.15%), Space: 51.1 MB (61.54%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cs
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cs
@@ -3,10 +3,8 @@ public class Solution {
         int n = nums.Length;
         int sum=0;
         int cnt = 0;
-        Dictionary<int,int> p= new Dictionary<int,int>();
+        int[] p= new int[k];
         p[0]=1;
-        for(int i=1;i<k;i++)
-            p[i]=0;
         for (int i = 0; i < n; i++)
         {
             sum += nums[i];
