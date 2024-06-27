f77fbdd2133c7eed8d659e1da6739127a963f29d
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-06-27 14:04:22 +0600

    Time: 382 ms (73.91%), Space: 68.9 MB (96.38%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cs
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cs
@@ -1,20 +1,9 @@
 public class Solution {
     public int FindCenter(int[][] edges) {
-        int n = edges.Length + 1;
-        int[] dgr = new int[n + 1];
-
-        foreach (int[] edge in edges) 
-        {
-            dgr[edge[0]]++;
-            dgr[edge[1]]++;
+         if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        } else {
+            return edges[0][1];
         }
-
-        for (int i = 1; i <= n; i++) 
-        {
-            if (dgr[i] == n - 1) 
-                return i;
-        }
-
-        return -1;
     }
 }
