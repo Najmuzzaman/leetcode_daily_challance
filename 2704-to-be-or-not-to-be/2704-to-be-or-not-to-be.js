9cc37f25899e01d13808a12082d8e6bc7e2c37b8
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-07-12 23:29:29 +0600

    Time: 51 ms (65.23%), Space: 49.1 MB (13.54%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -4,12 +4,12 @@
  */
 var expect = function(val) {
     return {
-        toBe: (v) =>
+        toBe: v =>
         {
             if(v===val) return true;
             else throw new Error("Not Equal");
         },
-        notToBe: (v) => 
+        notToBe: v => 
         {
             if(v!==val) return true;
             else throw new Error("Equal");
