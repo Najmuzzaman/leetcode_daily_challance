80903d369ad1382f5948a5e53a73b4809477253a
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-11-18 12:48:24 +0600

    Time: 0 ms (100.00%), Space: 45.7 MB (98.97%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cs
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cs
@@ -1,28 +1,28 @@
 public class Solution {
-    int n;
     public int[] Decrypt(int[] code, int k) {
-         n = code.Length;
-         int[] ans = new int[n];
-         for(int i=0;i<n;i++)
-         {
-             ans[i] = Sum(code, i, k);
-         }
-         return ans;
-    }
-    public int Sum(int[] code,int start, int k)
-    {
+         int n = code.Length;
+        int[] ans = new int[n];        
+        
+        if (k == 0) return ans;
+
         int sum = 0;
-        if (k > 0)
+        int start = k > 0 ? 1 : n + k;
+        int end = k > 0 ? k : n - 1;
+        
+
+        for (int i = start; i <= end; i++)
+            sum += code[i % n];
+        
+
+        for (int i = 0; i < n; i++)
         {
-            for (int i = 0; i < k; i++)
-                sum += code[(i + start + 1) % n];
+            ans[i] = sum;
+            sum -= code[start % n];
+            start++;
+            end++;
+            sum += code[end % n];  
         }
-        else
-        {
-            k=Math.Abs(k);
-            for (int i = 0; i < k; i++)
-                sum += code[(n+ start - 1 - i) % n];
-        }
-        return sum;
+
+        return ans;
     }
 }
