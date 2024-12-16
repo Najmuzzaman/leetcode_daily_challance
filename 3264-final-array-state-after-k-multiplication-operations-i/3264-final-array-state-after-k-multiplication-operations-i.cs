dda8afcb9d311f8837e80c86ab355d1660223c36
Md. Najmuzzaman <naj06cse@yahoo.com>
2024-12-16 20:23:09 +0600

    Time: 1 ms (100.00%), Space: 49.4 MB (66.22%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] GetFinalState(int[] nums, int k, int multiplier) {
+        int n=nums.Length;
+        for (int i = 0; i < k; i++)
+        {
+            int minIndex = 0;
+            for (int j = 1; j < n; j++)
+            {
+                if (nums[j] < nums[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            nums[minIndex] *= multiplier;
+        }
+
+        return nums;
+    }
+}
