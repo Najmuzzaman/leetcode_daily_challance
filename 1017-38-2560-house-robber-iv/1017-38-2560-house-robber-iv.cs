2c1a2308e933c921c2b8f26caa33d71ca3824869
Md. Najmuzzaman <naj06cse@yahoo.com>
2025-03-15 08:40:04 +0600

    Time: 26 ms, Memory: 63.3 MB - LeetHub

---
+++ 1017-38-2560-house-robber-iv/1017-38-2560-house-robber-iv.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int MinCapability(int[] nums, int k) {
+        int left = nums.Min();
+        int right = nums.Max();
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (CanStealKHouses(nums, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    public bool CanStealKHouses(int[] nums, int k, int capability) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.Length) {
+            if (nums[i] <= capability) {
+                count++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return count >= k;
+    }
+}
