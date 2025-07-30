01ef4be9546f13b838f6bdedb2664fc1c03cca8d
Md. Najmuzzaman <naj06cse@yahoo.com>
2025-07-30 09:37:35 +0600

    Time: 1 ms (100%), Space: 77 MB (80%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cs
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cs
@@ -1,21 +1,26 @@
 public class Solution {
     public int LongestSubarray(int[] nums) {
-        int maxVal = nums.Max();
-        int n = nums.Length;
-        
-        // Step 2: Find the longest subarray with bitwise AND equal to maxVal
-        int longestLength = 0;
-        int currentLength = 0;        
-        for (int i = 0; i < n; i++) 
+        int max = 0;
+        int count = 0;
+        int precord = 1;
+
+        for (int i=0; i<nums.Length; i++)
         {
-            if (nums[i] == maxVal)
+            if (nums[i] < max)            
+                count = 0;
+            if (nums[i] == max) 
             {
-                currentLength++;
-                longestLength = (longestLength>currentLength?longestLength:currentLength);
+                count++;
+                if (count > precord) precord = count;
             }
-            else
-                currentLength = 0;            
-        }        
-        return longestLength; 
+
+            if (nums[i] > max) // new max
+            {
+                max = nums[i];
+                count = 1;
+                precord = 1;
+            }
+        }
+        return precord;
     }
 }
