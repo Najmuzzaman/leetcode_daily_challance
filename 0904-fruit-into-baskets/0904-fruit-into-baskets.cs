7b64931c6be636ac01b241adc662e13e55c5e026
Md. Najmuzzaman <naj06cse@yahoo.com>
2025-08-04 17:03:01 +0600

    Time: 35 ms (27.43%), Space: 53.4 MB (94.09%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int TotalFruit(int[] fruits) {
+        Dictionary<int, int> basket = new Dictionary<int, int>();
+        int ans = 0;
+        int start = 0;
+
+        for (int end = 0; end < fruits.Length; end++) {
+            int fruit = fruits[end];
+            if (!basket.ContainsKey(fruit))
+                basket[fruit] = 0;
+            basket[fruit]++;
+
+            while (basket.Count > 2) {
+                int startFruit = fruits[start];
+                basket[startFruit]--;
+                if (basket[startFruit] == 0) {
+                    basket.Remove(startFruit);
+                }
+                start++;
+            }
+
+            ans = Math.Max(ans, end - start + 1);
+        }
+
+        return ans;
+    }
+}
