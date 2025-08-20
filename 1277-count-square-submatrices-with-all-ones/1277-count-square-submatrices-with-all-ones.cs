d353f9a258d4bf7f5a1c8448839f09fbfbda66bd
Md. Najmuzzaman <naj06cse@yahoo.com>
2025-08-20 11:01:50 +0600

    Time: 5 ms (81.25%), Space: 58.4 MB (72.92%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cs
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cs
@@ -1,25 +1,26 @@
 public class Solution {
     public int CountSquares(int[][] matrix) {
         int m = matrix.Length;
-        int n = matrix[0].Length;
+        int n = matrix[0].Length;        
         int[,] dp = new int[m, n];
-        int count = 0;
-
-        for (int i = 0; i < m; i++)
+        int cntSq = 0;
+        
+        for (int i = 0; i < m; i++) 
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++) 
             {
-                if (matrix[i][j] == 1)
+                if (matrix[i][j] == 1) 
                 {
                     if (i == 0 || j == 0) 
-                        dp[i, j] = 1;
-                    else
-                        dp[i, j] = Math.Min(Math.Min(dp[i - 1, j], dp[i, j - 1]), dp[i - 1, j - 1]) + 1;
+                        dp[i, j] = 1; 
+                    else 
+                        dp[i, j] = 1 + Math.Min(dp[i - 1, j],Math.Min(dp[i, j - 1], dp[i - 1, j - 1]));
                     
-                    count += dp[i, j];
+                    cntSq += dp[i, j];
                 }
             }
-        }        
-        return count;
+        }
+        
+        return cntSq;
     }
 }
