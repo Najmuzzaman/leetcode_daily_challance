7fddd6666d2f5a92529fac6ffb66b498bae9dc26
Md. Najmuzzaman <naj06cse@yahoo.com>
2025-11-18 10:14:35 +0600

    Time: 0 ms (100%), Space: 44.9 MB (48.89%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsOneBitCharacter(int[] bits) {
+        int n = bits.Length;
+        int i=0;
+        while(i<n-1){
+            if(bits[i] == 1){
+                i+=2;
+            }
+            else
+                i++;
+        }
+        return i<n;
+    }
+}
