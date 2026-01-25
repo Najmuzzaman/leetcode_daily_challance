fc891e4a768a9e41801ddb0dca89fba00bfa69a0
Md. Najmuzzaman <naj06cse@yahoo.com>
2026-01-25 13:57:42 +0600

    Time: 6 ms (98.65%), Space: 44.8 MB (86.49%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MinimumDifference(int[] nums, int k) {
+        if (nums.Length == 1)
+            return 0;
+    
+        Array.Sort(nums, (First, Second) => Second - First);
+        int ans = int.MaxValue;
+
+        for (int i = 0; i <= nums.Length - k; i++)
+            ans = Math.Min(ans, nums[i] - nums[i + k - 1]);
+
+        return ans;
+    }
+}
