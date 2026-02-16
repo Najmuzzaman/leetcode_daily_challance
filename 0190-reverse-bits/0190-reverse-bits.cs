3b31a18932126be57f2b7306697b377deb209743
Md. Najmuzzaman <naj06cse@yahoo.com>
2026-02-16 14:22:09 +0600

    Time: 18 ms (93.64%), Space: 29 MB (57.8%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int ReverseBits(int n) {
+         int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans = (ans << 1) | ((n >> i) & 1);
+        }
+        return ans;
+    }
+}
